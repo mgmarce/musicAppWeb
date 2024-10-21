cf95ac39a6a9a965ecb3e4b3523f42b4ce3bb6f2
mgmarce <https.mgmarce@gmail.com>
2024-10-21 14:38:53 -0600

    Arreglando img v1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,48 +1,55 @@
 import React from 'react';
 import Styles from '../styles/Home.module.css';
-//import img1 from '../../public/img/artista1.jpg'
+import img1 from '../../public/img/artista1.jpg'
+import img2 from '../../public/img/artista2.jpeg'
+import img3 from '../../public/img/artista5.jpg'
+import img4 from '../../public/img/artista4.jpeg'
+import img5 from '../../public/img/artista3.jpeg'
+import img6 from '../../public/img/artista11.jpeg'
+import img7 from '../../public/img/artista7.png'
+import img8 from '../../public/img/artista1.jpg'
 
 
 const imgData = [
     {
-      src: '../../public/img/artista1.jpg',
+      src: img1,
       alt: 'Artista 1',
       shadow: 'rgba(148, 163, 174, 0.837) 0 8px 16px'
     },
     {
-      src: '../../public/img/artista2.jpeg',
+      src: img2,
       alt: 'Artista 2',
       shadow: 'rgba(166, 192, 219, 0.837) 0 8px 16px'
     },
     {
-      src: '../../public/img/artista5.jpg',
+      src: img3,
       alt: 'Artista 3',
       shadow: 'rgba(168, 73, 75, 0.636) 0 8px 16px'
     },
     {
-      src: '../../public/img/artista4.jpeg',
+      src: img4,
       alt: 'Artista 4',
       shadow: 'rgba(178, 210, 255, 0.936) 0 8px 16px'
     },
     {
-      src: '../../public/img/artista3.jpeg',
+      src: img5,
       alt: 'Artista 5',
       shadow: 'rgba(154, 154, 154, 0.936) 0 8px 16px'
     },
     ,
     {
-      src: '../../public/img/artista11.jpeg',
+      src: img6,
       alt: 'Artista 6',
       shadow: 'rgba(249, 209, 135, 0.725) 0 8px 16px'
     },
     ,
     {
-      src: '../../public/img/artista7.png',
+      src: img7,
       alt: 'Artista 7',
       shadow: 'rgba(135, 211, 249, 0.725) 0 8px 16px'
     },
     {
-      src: '../../public/img/artista8.jpeg',
+      src: img8,
       alt: 'Artista 8',
       shadow: 'rgba(255, 228, 179, 0.725) 0 8px 16px'
     }
